b71c6313b8dc93b7afcc80dbbfb53270ee2443a9
Marlon Hugo <br.marlon.hugo@gmail.com>
2020-04-03 20:12:39 -0300

    Inicio do projeto

---
+++ ProjFutebol_01/Partida.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjFutebol_01
+{
+    class Partida
+    {
+        /* Atributos */
+        private DateTime data;
+        private Time timeCasa;
+        private Time timeVisitante;
+        private int placarCasa;
+        private int placarVisitante;
+
+        /*Construtor*/
+        public Partida(DateTime dataPartida, Time Tcasa, Time Tvisitante)
+        {
+            this.data          = dataPartida;
+            this.timeCasa      = Tcasa;
+            this.timeVisitante = Tvisitante;
+            this.placarCasa = 0;
+            this.placarVisitante = 0;
+        }
+
+        /*metodos*/
+        public int golCasa()
+        {
+            return placarCasa++;
+        }
+
+        public int golVisitante()
+        {
+            return placarVisitante++;
+        }
+        
+
+        public string getPalacar()
+        {
+            if(this.placarCasa > this.placarVisitante)
+            {
+                this.timeCasa.Vitorias = 1;
+                this.timeVisitante.Derrotas = 1;
+                return "O Vencedor da partida em " + this.data.ToString("dd-MM-yyyy") +
+                        " foi o time " + this.timeCasa.Nome + " com o placar de " +
+                        this.placarCasa + " a " +
+                        this.placarVisitante;
+            }
+
+            if (this.placarCasa == this.placarVisitante)
+            {
+                this.timeCasa.Empates = 1;
+                this.timeVisitante.Empates = 1;
+                return "Sem vencedor na partida em " + this.data.ToString("dd-MM-yyyy") +
+                        " com o placar de " + this.placarCasa + " a " +
+                        this.placarVisitante;
+            }
+
+
+            this.timeVisitante.Vitorias = 1;
+            this.timeCasa.Derrotas = 1;
+            return "O Vencedor da partida em " + this.data.ToString("dd-MM-yyyy") +
+                       " foi o time " + this.timeVisitante.Nome +
+                       " com o placar de " + this.timeVisitante + " a " +
+                       this.timeCasa;
+        }
+
+        public void jogar()
+        {
+            Random rand = new Random();
+            Int32 numeroDeGols = rand.Next(0, 15);
+
+            for (int i = 1; i <= numeroDeGols; i++)
+            {
+                Int32 golDoTime = rand.Next(0, 2);
+                if (golDoTime == 1)
+                    this.golCasa();
+                else
+                    this.golVisitante();
+            }
+        }
+
+    }
+}
---
+++ ProjFutebol_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjFutebol_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1 = new Time("Palmeiras");
+            Time time2 = new Time("Santos");
+
+            Time time3 = new Time("Atletico");
+            Time time4 = new Time("Cruzeiro");
+
+            Partida p1 = new Partida(DateTime.Now, time1, time2);
+            p1.jogar();
+            Console.WriteLine(p1.getPalacar());
+
+            Partida p2 = new Partida(DateTime.Now, time3, time4);
+            p2.jogar();
+            Console.WriteLine(p2.getPalacar());
+
+            Partida p3 = new Partida(DateTime.Now, time1, time3);
+            p3.jogar();
+            Console.WriteLine(p3.getPalacar());
+
+            Partida p4 = new Partida(DateTime.Now, time2, time4);
+            p4.jogar();
+            Console.WriteLine(p4.getPalacar());
+
+       
+            Console.WriteLine(time1.getStatus());
+            Console.WriteLine(time2.getStatus());
+            Console.WriteLine(time3.getStatus());
+            Console.WriteLine(time4.getStatus());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
